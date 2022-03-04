b495728594357a0c7b13ffbe490eb79782af06bf
Mr-coder-Tee <tebatsoterrence.manaka@gmail.com>
2022-03-04 02:41:03 +0200

    aa

--- src/cmpts/Reset.jsx
+++ src/cmpts/Reset.jsx
@@ -1,11 +1,70 @@
-import React from 'react'
+import React from "react";
+import Card from "@mui/material/Card";
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import Input from '@mui/material/Input';
+import FilledInput from '@mui/material/FilledInput';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormHelperText from '@mui/material/FormHelperText';
+import FormControl from '@mui/material/FormControl';
+import TextField from '@mui/material/TextField';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
 
 const Reset = () => {
+    const [values, setValues] = React.useState({
+        password: '',
+        showPassword: false,
+      });
+      const handleClickShowPassword = () => {
+        setValues({
+          ...values,
+          showPassword: !values.showPassword,
+        });
+      };
+      const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+      };
+      const handleChange = (prop) => (event) => {
+        setValues({ ...values, [prop]: event.target.value });
+      };
   return (
-    <div>
-        akakaka
-    </div>
-  )
-}
+    <Card sx={{ maxWidth: 545 }}>
+      <Box
+        component="form"
+        sx={{
+          "& > :not(style)": { m: 1, width: "25ch" }
+        }}
+        noValidate
+        autoComplete="off"
+      >
+         <FormControl sx={{ m: 1, width: '25ch' }} variant="outlined">
+          <InputLabel htmlFor="outlined-adornment-password">Password</InputLabel>
+          <OutlinedInput
+            id="outlined-adornment-password"
+            type={values.showPassword ? 'text' : 'password'}
+            value={values.password}
+            onChange={handleChange('password')}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {values.showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+            label="Password"
+          />
+        </FormControl> 
+          </Box>
+    </Card>
+  );
+};
 
-export default Reset
+export default Reset;
