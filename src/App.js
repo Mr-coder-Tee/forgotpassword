e9e3673fc11355ccd83cce8086c65c7c9865119a
Mr-coder-Tee <tebatsoterrence.manaka@gmail.com>
2022-03-04 04:58:34 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     <BrowserRouter>
     <div className="App">
       <Routes>
-      <Route path="/" element={<Navigate to={`/resetpassword`} replace={true}/>}/>
+      <Route path="/" element={<Navigate to={`/resetpassword/:id`} replace={true}/>}/>
       {/* <Route   path="/" element={<Reset />}/> */}
       <Route  path="/resetpassword/:id" element={<Reset />} />
       </Routes>
